109c21e5deab569058d21209ea54afa020def3c2
jfparedesutpl <jfparedes@utpl.edu.ec>
2024-05-07 22:05:19 -0500

    Update practica.py

--- practica.py
+++ practica.py
@@ -1,32 +1,19 @@
-import requests
 import pandas as pd
+import requests
 
-# Consumir el servicio HTTP para una frase específica
-def obtener_prediccion(frase):
-    url = f'http://127.0.0.1:8080/prediccion/{frase}'
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        prediction = response.json()['prediction']
-        return prediction
-    else:
-        return f'Error al hacer la solicitud HTTP para la frase "{frase}": {response.status_code}'
+def obtener_sentimiento(frase):
+    url = "http://127.0.0.1:8080/prediccion/" + frase
+    response = requests.get(url) #Petición del servicio
+    data = response.json()
+    return data['prediction']
 
-# Leer un archivo CSV que contiene frases
-def leer_csv_y_predecir(archivo_csv):
-    # Lee el archivo CSV
-    df = pd.read_csv(archivo_csv)
-    
-    # Itera sobre las filas del DataFrame y hace solicitudes HTTP para cada frase
-    for index, row in df.iterrows():
-        frase = row['Frase']
-        prediction = obtener_prediccion(frase)
-        print(f'Frase: "{frase}", Resultado: {prediction}')
-
-# Función principal
 def main():
-    archivo_csv = 'frases.csv'
-    leer_csv_y_predecir(archivo_csv)
+    archivo_csv = "Herramientas Practica semana 5/frases.csv"
+    df = pd.read_csv(archivo_csv, names=["Frase"], header=None)  # Leer el CSV con Pandas
+    for index, row in df.iterrows():
+        frase = row["Frase"]
+        resultado = obtener_sentimiento(frase)
+        print(f"Frase: {frase}, Resultado: {resultado}")
 
 if __name__ == "__main__":
     main()
